1c4b7f43b1c9ae4c9e5b8e810509cb804e2620ec
Shubham Dalvi <shubham.dalvi97@gmail.com>
2018-11-22 00:38:24 +0530

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -3,22 +3,17 @@ import java.security.NoSuchAlgorithmException;
 import java.nio.charset.StandardCharsets;
 import java.math.BigInteger;
 
-
 class Hashing
-
-{
+{	
 	public static void main(String args[]) throws Exception
 	{
-	
-	MessageDigest md = MessageDigest.getInstance( "SHA-256" );
-    String text = "Text to hash, cryptographically.";
+		MessageDigest md = MessageDigest.getInstance( "SHA-256" );
+    		String text = "Text to hash, cryptographically.";
     
-    md.update( text.getBytes( StandardCharsets.UTF_8 ) );
-    byte[] digest = md.digest();
+    		md.update( text.getBytes( StandardCharsets.UTF_8 ) );
+    		byte[] digest = md.digest();
 
-    String hex = String.format( "%064x", new BigInteger( 1, digest ) );
-    System.out.println("Hash: "+ hex );
-
-	
+    		String hex = String.format( "%064x", new BigInteger( 1, digest ) );
+    		System.out.println("Hash: "+ hex );
 	}
 }
